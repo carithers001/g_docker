cb09390c5e3e6ecaf17cd1fff7428ede1332384a
carithers <carithers@163.com>
2026-09-18 15:05:30 +0800

    ENV_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ if [ "$IPV" != "4" ] && [ "$IPV" != "6" ]; then
     exit 1
 fi
 
-# 检查是否配置了 envToken 环境变量
-if [ -z "$envToken" ]; then
-    echo "[-] 致命错误: 未检测到环境变量 envToken！请在云平台设置该变量。"
+# 检查是否配置了 ENV_TOKEN 环境变量
+if [ -z "$ENV_TOKEN" ]; then
+    echo "[-] 致命错误: 未检测到环境变量 ENV_TOKEN！请在云平台设置该变量。"
     exit 1
 fi
 
@@ -46,7 +46,7 @@ echo "[cloudflared] 检查更新并启动固定隧道..."
     --edge-ip-version "$IPV" \
     --protocol http2 \
     --metrics "0.0.0.0:$HEALTH_PORT" \
-    tunnel run --token "$envToken" &
+    tunnel run --token "$ENV_TOKEN" &
 
 echo "========================================"
 echo "已连接到 Cloudflare Zero Trust (Token 硬编码模式)"
